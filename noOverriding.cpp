71bb2d2e7680a36ab001606c4d8997c9a010598f
nurrizqifp <nurrizqifebriansyah@gmail.com>
2025-06-03 15:27:07 +0700

    Membuat program utama

--- noOverriding.cpp
+++ noOverriding.cpp
@@ -14,3 +14,10 @@ class derivedClass : public baseClass {
             cout << "Hallo saya Function dari derived class";
         }
 };
+
+int main() {
+    derivedClass a;
+    a.perkenalan();
+
+    return 0;
+}
