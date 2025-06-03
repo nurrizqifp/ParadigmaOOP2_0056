855e08f903ca96e4b98d538033db6f09a87026a5
nurrizqifp <nurrizqifebriansyah@gmail.com>
2025-06-03 14:48:52 +0700

    Membuat void setXY dengan parameter string a, string b

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -3,4 +3,13 @@ using namespace std;
 
 class AbstraksiKlas {
     private: string x, y;
+
+    public:
+
+        // method untuk mengisi nilai
+        // private member
+        void setXY(string a, string b) {
+            x =  a;
+            y = b;
+        }
 };
