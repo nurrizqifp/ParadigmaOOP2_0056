c9a3b95b16e7e77785274b70de5e53fe1b7b8792
nurrizqifp <nurrizqifebriansyah@gmail.com>
2025-06-03 14:30:12 +0700

    Membuat class turunan bernama lia

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -15,3 +15,10 @@ class joko : public seseorang {
             cout << "Pesan dari joko" << endl;
         }
 };
+
+class lia : public seseorang {
+    public:
+        void pesan() {
+            cout << "Pesan dari lia" << endl;
+        }
+};
