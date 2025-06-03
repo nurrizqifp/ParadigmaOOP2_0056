9527f7dfafa0d37574b61264f963067a19d28ec2
nurrizqifp <nurrizqifebriansyah@gmail.com>
2025-06-03 14:49:24 +0700

    Membuat void display

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -12,4 +12,10 @@ class AbstraksiKlas {
             x =  a;
             y = b;
         }
+
+        // menampilkan nilai
+        void display() {
+            cout << "x = " << x << endl;
+            cout << "y = " << y << endl;
+        }
 };
