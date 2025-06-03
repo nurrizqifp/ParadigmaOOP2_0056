e7233c6966aa227b708e7cf412b658d75b2171d5
nurrizqifp <nurrizqifebriansyah@gmail.com>
2025-06-03 14:55:15 +0700

    Membuat program utama

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -12,3 +12,12 @@ class remoteLampu {
             return saklarNo[i];
         }
 };
+
+int main() {
+    remoteLampu lampuRumah;
+
+    lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
+    lampuRumah.setSaklarNo(1, "Lampu Ruang Tamu");
+    lampuRumah.setSaklarNo(2, "Lampu Kamar Tidur");
+    lampuRumah.setSaklarNo(3, "Lampu Dapur");
+}
